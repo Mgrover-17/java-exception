1f3b67a9390f24b717c79c0b34ac0907b48f4682
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-03-30 23:12:18 +0530

    [MANVI GROVER] ADD: Validate age by creating custom exception.

---
+++ src/main/java/com/bridgelabz/javaException/AgeValidator.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.javaException;
+import java.util.*;
+
+//custom exception
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+public class AgeValidator {
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+        System.out.println("Access granted!");
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter your age: ");
+            int age = scanner.nextInt();
+            validateAge(age);
+        }
+        catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println("Invalid input. Please enter a valid age.");
+        }
+        finally {
+            scanner.close();
+            System.out.println("Program execution completed.");
+        }
+    }
+}
